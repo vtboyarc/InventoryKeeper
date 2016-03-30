aa7c142fa311dde58938c40195feff8f1e8fb2b3
vtboyarc <adamrcarter@gmail.com>
2016-03-30 18:10:17 -0500

    first commit

---
+++ Homepwner/Item.swift
@@ -0,0 +1,49 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Adam Carter on 3/28/16.
+//  Copyright © 2016 Adam Carter. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Big", "Raunchy", "Nasty"]
+            let nouns = ["Goon", "Savage", "Beast"]
+            
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(idx)]
+            
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = NSUUID().UUIDString.componentsSeparatedByString("-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        }
+        else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+        
+    }
+    
+}
---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Adam Carter on 3/28/16.
+//  Copyright © 2016 Adam Carter. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        //that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+}
